322acabece1446a3caa98ba72c9e95c1de7909dc
Zaka-En <zakariaennahouti@gmail.com>
2026-02-11 14:02:32 +0100

    feat: update the broadcaster to use a redis database instead of memory allowing muliprocessing to have centralized broadcast channel to pub/sub

--- fast-graphql/app/schema/types.py
+++ fast-graphql/app/schema/types.py
@@ -6,7 +6,7 @@ from base64 import b64encode, b64decode
 from typing import TypeVar
 from broadcaster import Broadcast
 
-broadcast = Broadcast("memory://")
+broadcast = Broadcast("redis://localhost:6379")
 
 @strawberry.type
 class AuthorType(strawberry.relay.Node):
