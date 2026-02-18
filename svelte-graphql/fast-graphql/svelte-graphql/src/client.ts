35504783d6c848a573b183d4e8a2755d25ed9b03
Zaka-En <zakariaennahouti@gmail.com>
2026-02-18 18:02:58 +0100

    chore: added a commented claudecode-generated plugin to later try it

--- svelte-graphql/fast-graphql/svelte-graphql/src/client.ts
+++ svelte-graphql/fast-graphql/svelte-graphql/src/client.ts
@@ -4,7 +4,135 @@ import { createClient } from 'graphql-ws';
 
 //let refreshPromise: Promise<Response> | null = null;
 const pendingAuthRetries: (Promise<Response>)[] = [];
+type Resolver = (value: Response) => void
+const pendingAuthRetries: Resolver[] = []
 let isWaitingForReauth = false;
+const API_URL = 'http://localhost:8000/graphql';
+
+
+// ESTA ES UN CHOARRADA QUE HA HECHO CLAUDE
+// const customFetch: typeof fetch = async (input, init) => {
+//     let response = await fetch(input, init)
+
+//     if (response.status != 401) return response
+
+//     if (!isWaitingForReauth) {
+        
+//         isWaitingForReauth = true
+
+//         const refreshResult = await fetch(API_URL, {
+//             method: 'POST',
+//             headers: { 'Content-Type': 'application/json' },
+//             credentials: 'include',
+//             body: JSON.stringify({ query: `mutation RefreshToken { refreshToken { accessToken } }` })
+//         });
+
+//         isWaitingForReauth = false
+
+//         if (refreshResult.ok) {
+//             const retryResponse = await fetch(input, init)
+            
+//             const resolvers = [...pendingAuthRetries]
+//             pendingAuthRetries.length = 0;
+//             for(const resolve of resolvers){
+//                 resolve(new Response()) // <--- Esta linea no sirve para nada
+//             }
+
+//             return retryResponse
+//         }else{
+//             pendingAuthRetries.length = 0
+//             return refreshResult
+//         }
+//     }
+    //     else {
+    //         // Ya hay un refresh en curso, me encolo y espero
+    //         const waitPromise = new Promise<Response>((resolve) => {
+    //             pendingAuthRetries.push(resolve);
+    //         });
+
+    //         await waitPromise; // espero a que el primero termine el refresh
+
+    //         // Cuando me despiertan, reintento mi petición original
+    //         return fetch(input, init);
+    //     }
+    // };
+// }
+
+// const pendingAuthRetries: (Promise<Response>)[] = [];
+// let isWaitingForReauth = false;
+
+// const customFetch: typeof fetch = async (input, init) => {
+//     // 1. Log inicial para confirmar que el fetch está entrando aquí
+//     console.log("Houdini: Petición iniciada a", (input as Request).url || input);
+
+//     let response = await fetch(input, init);
+
+//     let isUnauthorized = false;
+
+//     if (response.status == 401) { //  402 403
+//         isUnauthorized = true;
+
+//         try {
+//             const responseBody = await response.json();
+//             if (responseBody?.errors) {
+//                 console.error(responseBody?.errors)
+//             }
+//         } catch {
+//             // No es JSON, seguimos
+//         }
+//     } else {
+//         return response;
+//     }
+
+//     // 4. --- LÓGICA DE REFRESH ---
+//     if (isUnauthorized) {
+
+//         if (!isWaitingForReauth) {
+//             console.log("Houdini: Lanzando Silent Refresh...");
+//             isWaitingForReauth = true;
+//             const refreshResolt = await fetch(API_URL, {
+//                 method: 'POST',
+//                 headers: { 'Content-Type': 'application/json' },
+//                 credentials: 'include',
+//                 body: JSON.stringify({
+//                     query: `mutation RefreshToken { refreshToken { accessToken } }`
+//                 })
+//             });
+//             if (refreshResolt.ok) {
+//                 if (pendingAuthRetries.length) {
+//                     Promise.all(pendingAuthRetries)
+//                 }
+//                 // haz ok
+//             } else {
+//                 //error en todos los promise
+//             }
+//         } else {
+//             const waitingPromise = new Promise(() =)
+
+//             pendingAuthRetries.push(waitingPromise)
+
+
+//             try {
+//                 const refreshResponse = await waitingPromise;
+
+//                 if (refreshResponse?.ok) {
+//                     console.log("Houdini: Refresh exitoso. Reintentando mutación original...");
+//                     // Reintentamos la petición original
+//                     return fetch(input, init);
+//                 }
+//             }
+//             } catch (err) {
+//             console.error("Houdini: Error crítico en el refresh", err);
+//             return
+//         }
+//     }
+// }
+
+// return response;
+// };
+
+
+let refreshPromise: Promise<Response> | null = null;
 
 
 const API_URL = 'http://localhost:8000/graphql';
@@ -75,7 +203,6 @@ const customFetch: typeof fetch = async (input, init) => {
             console.error("Houdini: Error crítico en el refresh", err);
         }
 
-        
     }
 
     return response;
@@ -157,12 +284,12 @@ export default new HoudiniClient({
     fetchParams() {
         return { credentials: 'include' };
     },
-    
+
     //houdini functions/plugins pipeline
     plugins: [
         () => ({
             network: async (ctx, { next }) => {
-                
+
                 ctx.fetch = customFetch;
                 return next(ctx);
             },
