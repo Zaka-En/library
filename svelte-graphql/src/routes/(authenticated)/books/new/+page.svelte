0329b3dfca9d5de8143c9f988d795eba26ea037d
Zaka-En <zakariaennahouti@gmail.com>
2026-03-17 16:39:13 +0100

    fix: update Book comp

--- svelte-graphql/src/routes/(authenticated)/books/new/+page.svelte
+++ svelte-graphql/src/routes/(authenticated)/books/new/+page.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
-  import BookForm from "$lib/components/BookForm.svelte";
-  import type { PageProps } from "./$types";
+    import BookForm from "$lib/components/BookForm.svelte";
+    import type { PageProps } from "./$types";
 
-  interface AuthorNameId{
-    readonly id: string;
-    readonly name: string;
-  }
+    interface AuthorNameId {
+        readonly id: string;
+        readonly name: string;
+    }
 
-  let { data } : PageProps = $props()
-  let store =  $derived( data.authorNamesStore)
-  let authorNames = $derived($store.data?.authorsQuery ?? []) as AuthorNameId[]
+    let { data }: PageProps = $props();
+    let store = $derived(data.authorNamesStore);
+    let authorNames = $derived(
+        $store.data?.authorsQuery ?? [],
+    ) as AuthorNameId[];
 </script>
 
-<BookForm {authorNames} book = {null} author={null} />
+<BookForm {authorNames} book={null} author={null} />
