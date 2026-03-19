c15e3c9b124132aa779a5e85a21ae7357c6e78ee
Zaka-En <zakariaennahouti@gmail.com>
2026-03-19 11:35:25 +0100

    style: format the types.ts file

--- comp-library/src/lib/components/RoomBooking/types.ts
+++ comp-library/src/lib/components/RoomBooking/types.ts
@@ -1,12 +1,11 @@
-
-export interface BaseRoom{
-  id: number | string
-  name: string
-  capacity: number
-  availableHours?: number[]
+export interface BaseRoom {
+	id: number | string;
+	name: string;
+	capacity: number;
+	availableHours?: number[];
 }
 
-export interface BookingRoomProvider<TRoom extends BaseRoom>{
-  getAvailableHours: (room: TRoom, date: string, startingHour: number) => Promise<number[]>
-  bookRoom: (input: any) => Promise<{success: boolean, error?: string}>
+export interface BookingRoomProvider<TRoom extends BaseRoom> {
+	getAvailableHours: (room: TRoom, date: string, startingHour: number) => Promise<number[]>;
+	bookRoom: (input: any) => Promise<{ success: boolean; error?: string }>;
 }
