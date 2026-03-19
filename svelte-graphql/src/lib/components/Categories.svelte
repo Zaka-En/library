606508812ae8691bd2725a613bb426a1b4b9e24d
Zaka-En <zakariaennahouti@gmail.com>
2026-03-19 17:58:11 +0100

    style: format the category Component

--- svelte-graphql/src/lib/components/Categories.svelte
+++ svelte-graphql/src/lib/components/Categories.svelte
@@ -1,32 +1,24 @@
 <script lang="ts">
-
   import type { Snippet } from "svelte";
 
-  interface Props{
-    categories: CategoryType[]
-    categorySnippet: Snippet<[string,string,number]>
-    children: Snippet<[CategoryType,Snippet<[string,string,number]>]>
+  interface Props {
+    categories: CategoryType[];
+    categorySnippet: Snippet<[string, string, number]>;
+    children: Snippet<[CategoryType, Snippet<[string, string, number]>]>;
   }
-  
 
-  let { categories, categorySnippet, children} : Props = $props();
-  
-
-
+  let { categories, categorySnippet, children }: Props = $props();
 </script>
 
 <aside class=" w-3/12 h-auto flex flex-col gap-2">
-   
   {#if children}
-    {#each categories as cat }
-      {@render children(cat,categorySnippet)}
+    {#each categories as cat}
+      {@render children(cat, categorySnippet)}
     {/each}
   {:else}
-	  <p>fallback content</p>
+    <p>fallback content</p>
   {/if}
-  
-  
-  
+
   <!-- {#each categories as cat}
     <div class="group bg-white p-4 rounded-lg border border-gray-100 ">
       {@render categorySnippet(cat.name, cat.description, cat.totalBooks)}
